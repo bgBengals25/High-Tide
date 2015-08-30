3484a21d1a90dc9d37596c8153ff052aea3d501b
Peter Toth <ptoth585@gmail.com>
2015-08-30 17:59:46 -0400

    Crafting UI

--- src/com/hightide/Main.java
+++ src/com/hightide/Main.java
@@ -1,5 +1,7 @@
 package com.hightide;
 
+import com.hightide.ui.EditorUI;
+
 /**
  * Created by peter on 8/30/15.
  */
@@ -7,6 +9,6 @@ public class Main {
 
     public static void main(String [] args){
 
-
+        EditorUI window = new EditorUI();
     }
 }
--- src/com/hightide/ui/EditorUI.java
+++ src/com/hightide/ui/EditorUI.java
@@ -1,6 +1,7 @@
 package com.hightide.ui;
 
 import javax.swing.*;
+import java.awt.*;
 
 /**
  * Created by peter on 8/30/15.
@@ -9,17 +10,41 @@ public class EditorUI extends JFrame {
 
     private JTextArea textArea;
 
-    public int WINDOW_WIDTH = 800;
-    public int WINDOW_HEIGHT = 600;
+    public int WINDOW_WIDTH = 700;
+    public int WINDOW_HEIGHT = 450;
     public String WINDOW_INIT_TITLE = "High Tide Scripting Editor";
 
     public EditorUI(){
 
+        // Set up Menu Bar
         JMenuBar menuBar = new JMenuBar();
         setJMenuBar(menuBar);
+        // Set up Menu
+        JMenu fileMenu = new JMenu("File");
+        menuBar.add(fileMenu);
+        JMenuItem miNew = new JMenuItem("New");
+        fileMenu.add(miNew);
+        JMenuItem miOpen = new JMenuItem("Open");
+        fileMenu.add(miOpen);
+        JMenuItem miSave = new JMenuItem("Save");
+        fileMenu.add(miSave);
+        fileMenu.addSeparator();
+        JMenuItem miExit = new JMenuItem("Exit");
 
+
+
+        // Frame set up
+        Container container = getContentPane();
+        container.setLayout(new BorderLayout());
+        textArea = new JTextArea();
+        container.add(new JScrollPane(textArea), BorderLayout.CENTER);
+
+
+
+        // Initialize the Frame
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+        setLocationRelativeTo(null);
         setTitle(WINDOW_INIT_TITLE);
         setVisible(true);
     }
