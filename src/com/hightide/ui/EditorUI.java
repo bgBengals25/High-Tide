b1ccc4bb500717c01b87ede09638de21d15f4834
Peter Toth <ptoth585@gmail.com>
2015-08-30 17:47:32 -0400

    All Set Up

--- src/com/hightide/ui/EditorUI.java
+++ src/com/hightide/ui/EditorUI.java
@@ -7,5 +7,20 @@ import javax.swing.*;
  */
 public class EditorUI extends JFrame {
 
+    private JTextArea textArea;
 
+    public int WINDOW_WIDTH = 800;
+    public int WINDOW_HEIGHT = 600;
+    public String WINDOW_INIT_TITLE = "High Tide Scripting Editor";
+
+    public EditorUI(){
+
+        JMenuBar menuBar = new JMenuBar();
+        setJMenuBar(menuBar);
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+        setTitle(WINDOW_INIT_TITLE);
+        setVisible(true);
+    }
 }
