3afd7911d756e1c7547ae5729542ffb0c9f5a4aa
Peter Toth <ptoth585@gmail.com>
2015-08-31 14:09:21 -0400

    Working on Themes

--- src/com/hightide/highlight/syntax/theme/Theme.java
+++ src/com/hightide/highlight/syntax/theme/Theme.java
@@ -27,11 +27,107 @@ public class Theme {
     public Theme(String name){
         NAME = name;
         THEME_FILE = null;
-        loadTheme();
+    }
+
+    public Font getPLAIN_TEXT() {
+        return PLAIN_TEXT;
+    }
+
+    public void setPLAIN_TEXT(Font PLAIN_TEXT) {
+        this.PLAIN_TEXT = PLAIN_TEXT;
+    }
+
+    public Color getPLAIN_TEXT_COLOR() {
+        return PLAIN_TEXT_COLOR;
+    }
+
+    public void setPLAIN_TEXT_COLOR(Color PLAIN_TEXT_COLOR) {
+        this.PLAIN_TEXT_COLOR = PLAIN_TEXT_COLOR;
+    }
+
+    public Color getKEYWORD_TEXT_COLOR() {
+        return KEYWORD_TEXT_COLOR;
+    }
+
+    public void setKEYWORD_TEXT_COLOR(Color KEYWORD_TEXT_COLOR) {
+        this.KEYWORD_TEXT_COLOR = KEYWORD_TEXT_COLOR;
+    }
+
+    public Font getKEYWORD_TEXT() {
+        return KEYWORD_TEXT;
+    }
+
+    public void setKEYWORD_TEXT(Font KEYWORD_TEXT) {
+        this.KEYWORD_TEXT = KEYWORD_TEXT;
+    }
+
+    public Font getOS_KEYWORD_TEXT() {
+        return OS_KEYWORD_TEXT;
+    }
+
+    public void setOS_KEYWORD_TEXT(Font OS_KEYWORD_TEXT) {
+        this.OS_KEYWORD_TEXT = OS_KEYWORD_TEXT;
+    }
+
+    public Color getOS_KEYWORD_TEXT_COLOR() {
+        return OS_KEYWORD_TEXT_COLOR;
+    }
+
+    public void setOS_KEYWORD_TEXT_COLOR(Color OS_KEYWORD_TEXT_COLOR) {
+        this.OS_KEYWORD_TEXT_COLOR = OS_KEYWORD_TEXT_COLOR;
+    }
+
+    public Font getQUOTE_TEXT() {
+        return QUOTE_TEXT;
+    }
+
+    public void setQUOTE_TEXT(Font QUOTE_TEXT) {
+        this.QUOTE_TEXT = QUOTE_TEXT;
+    }
+
+    public Color getQUOTE_TEXT_COLOR() {
+        return QUOTE_TEXT_COLOR;
+    }
+
+    public void setQUOTE_TEXT_COLOR(Color QUOTE_TEXT_COLOR) {
+        this.QUOTE_TEXT_COLOR = QUOTE_TEXT_COLOR;
+    }
+
+    public Color getCOMMENT_TEXT_COLOR() {
+        return COMMENT_TEXT_COLOR;
+    }
+
+    public void setCOMMENT_TEXT_COLOR(Color COMMENT_TEXT_COLOR) {
+        this.COMMENT_TEXT_COLOR = COMMENT_TEXT_COLOR;
+    }
+
+    public Font getCOMMENT_TEXT() {
+        return COMMENT_TEXT;
+    }
+
+    public void setCOMMENT_TEXT(Font COMMENT_TEXT) {
+        this.COMMENT_TEXT = COMMENT_TEXT;
+    }
+
+    public String getNAME() {
+        return NAME;
+    }
+
+    public void setNAME(String NAME) {
+        this.NAME = NAME;
+    }
+
+    public File getTHEME_FILE() {
+        return THEME_FILE;
+    }
+
+    public void setTHEME_FILE(File THEME_FILE) {
+        this.THEME_FILE = THEME_FILE;
     }
 
     public Theme(File themefile){
         NAME = null;
+
         THEME_FILE = themefile;
     }
 
@@ -42,53 +138,5 @@ public class Theme {
 
     public void loadTheme(){
         ThemeLoader themeLoader = new ThemeLoader(this);
-    }
-
-    public Font getPLAIN_TEXT() {
-        return PLAIN_TEXT;
-    }
-
-    public Color getPLAIN_TEXT_COLOR() {
-        return PLAIN_TEXT_COLOR;
-    }
-
-    public Font getKEYWORD_TEXT() {
-        return KEYWORD_TEXT;
-    }
-
-    public Color getKEYWORD_TEXT_COLOR() {
-        return KEYWORD_TEXT_COLOR;
-    }
-
-    public Font getOS_KEYWORD_TEXT() {
-        return OS_KEYWORD_TEXT;
-    }
-
-    public Color getOS_KEYWORD_TEXT_COLOR() {
-        return OS_KEYWORD_TEXT_COLOR;
-    }
-
-    public Font getQUOTE_TEXT() {
-        return QUOTE_TEXT;
-    }
-
-    public Color getQUOTE_TEXT_COLOR() {
-        return QUOTE_TEXT_COLOR;
-    }
-
-    public Font getCOMMENT_TEXT() {
-        return COMMENT_TEXT;
-    }
-
-    public Color getCOMMENT_TEXT_COLOR() {
-        return COMMENT_TEXT_COLOR;
-    }
-
-    public String getNAME() {
-        return NAME;
-    }
-
-    public File getTHEME_FILE() {
-        return THEME_FILE;
     }
 }
--- src/com/hightide/highlight/syntax/theme/ThemeLoader.java
+++ src/com/hightide/highlight/syntax/theme/ThemeLoader.java
@@ -1,25 +1,95 @@
 package com.hightide.highlight.syntax.theme;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.awt.*;
+import java.io.File;
+
 /**
  * Created by peter on 8/31/15.
  */
 public class ThemeLoader {
 
     private Theme THEME;
+    private File themeFile;
 
     public ThemeLoader(Theme theme){
-
+        THEME = theme;
+        themeFile = theme.getTHEME_FILE();
     }
 
-    public void getPlainText(){
+    private void load(){
+        try {
 
-    }
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.parse(themeFile);
 
-    public void getKeywordText(){
+            doc.getDocumentElement().normalize();
 
-    }
+            NodeList nList = doc.getElementsByTagName("font");
 
-    public void getOSKeyWordText(){
+            for (int i=0; i< nList.getLength(); i++){
 
+                Node n = nList.item(i);
+                Element e = (Element)n;
+
+                if (e.getAttribute("name").equals("plaintext")){
+                    if(e.getAttribute("type").equals("normal")){
+                        THEME.setPLAIN_TEXT(new Font(e.getAttribute("family"), Font.PLAIN, Integer.parseInt(e.getAttribute("size"))));
+                    }else if(e.getAttribute("type").equals("bold")){
+                        THEME.setPLAIN_TEXT(new Font(e.getAttribute("family"), Font.BOLD, Integer.parseInt(e.getAttribute("size"))));
+                    }else if(e.getAttribute("type").equals("italics")){
+                        THEME.setPLAIN_TEXT(new Font(e.getAttribute("family"), Font.ITALIC, Integer.parseInt(e.getAttribute("size"))));
+                    }
+                    THEME.setPLAIN_TEXT_COLOR(Color.getColor(e.getAttribute("color")));
+                }else if (e.getAttribute("name").equals("keywordtext")){
+                    if(e.getAttribute("type").equals("normal")){
+                        THEME.setKEYWORD_TEXT(new Font(e.getAttribute("family"), Font.PLAIN, Integer.parseInt(e.getAttribute("size"))));
+                    }else if(e.getAttribute("type").equals("bold")){
+                        THEME.setKEYWORD_TEXT(new Font(e.getAttribute("family"), Font.BOLD, Integer.parseInt(e.getAttribute("size"))));
+                    }else if(e.getAttribute("type").equals("italics")){
+                        THEME.setKEYWORD_TEXT(new Font(e.getAttribute("family"), Font.ITALIC, Integer.parseInt(e.getAttribute("size"))));
+                    }
+                    THEME.setKEYWORD_TEXT_COLOR(Color.getColor(e.getAttribute("color")));
+                }else if (e.getAttribute("name").equals("oskeywordtext")){
+                    if(e.getAttribute("type").equals("normal")){
+                        THEME.setOS_KEYWORD_TEXT(new Font(e.getAttribute("family"), Font.PLAIN, Integer.parseInt(e.getAttribute("size"))));
+                    }else if(e.getAttribute("type").equals("bold")){
+                        THEME.setOS_KEYWORD_TEXT(new Font(e.getAttribute("family"), Font.BOLD, Integer.parseInt(e.getAttribute("size"))));
+                    }else if(e.getAttribute("type").equals("italics")){
+                        THEME.setOS_KEYWORD_TEXT(new Font(e.getAttribute("family"), Font.ITALIC, Integer.parseInt(e.getAttribute("size"))));
+                    }
+                    THEME.setOS_KEYWORD_TEXT_COLOR(Color.getColor(e.getAttribute("color")));
+                }else if (e.getAttribute("name").equals("quotetext")){
+                    if(e.getAttribute("type").equals("normal")){
+                        THEME.setQUOTE_TEXT(new Font(e.getAttribute("family"), Font.PLAIN, Integer.parseInt(e.getAttribute("size"))));
+                    }else if(e.getAttribute("type").equals("bold")){
+                        THEME.setQUOTE_TEXT(new Font(e.getAttribute("family"), Font.BOLD, Integer.parseInt(e.getAttribute("size"))));
+                    }else if(e.getAttribute("type").equals("italics")){
+                        THEME.setQUOTE_TEXT(new Font(e.getAttribute("family"), Font.ITALIC, Integer.parseInt(e.getAttribute("size"))));
+                    }
+                    THEME.setQUOTE_TEXT_COLOR((Color.getColor(e.getAttribute("color"))));
+                }else if (e.getAttribute("name").equals("comment")){
+                    if(e.getAttribute("type").equals("normal")){
+                        THEME.setCOMMENT_TEXT(new Font(e.getAttribute("family"), Font.PLAIN, Integer.parseInt(e.getAttribute("size"))));
+                    }else if(e.getAttribute("type").equals("bold")){
+                        THEME.setCOMMENT_TEXT(new Font(e.getAttribute("family"), Font.BOLD, Integer.parseInt(e.getAttribute("size"))));
+                    }else if(e.getAttribute("type").equals("italics")){
+                        THEME.setCOMMENT_TEXT(new Font(e.getAttribute("family"), Font.ITALIC, Integer.parseInt(e.getAttribute("size"))));
+                    }
+
+                    THEME.setCOMMENT_TEXT_COLOR(Color.getColor(e.getAttribute("color")));
+                }
+            }
+
+        }catch(Exception e){
+            System.out.println("Failed to load theme '"+THEME.getNAME()+"'");
+        }
     }
 }
