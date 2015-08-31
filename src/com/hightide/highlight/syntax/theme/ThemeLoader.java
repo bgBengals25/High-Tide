245f378703f6c663a07793817d35562d556653fe
Peter Toth <ptoth585@gmail.com>
2015-08-31 13:21:35 -0400

    Begin Crafting Themes

--- src/com/hightide/highlight/syntax/theme/ThemeLoader.java
+++ src/com/hightide/highlight/syntax/theme/ThemeLoader.java
@@ -20,6 +20,4 @@ public class ThemeLoader {
     public void getOSKeyWordText(){
 
     }
-
-    public void get
 }
---
+++ src/com/hightide/highlight/syntax/theme/Themes.java
@@ -0,0 +1,8 @@
+package com.hightide.highlight.syntax.theme;
+
+/**
+ * Created by peter on 8/31/15.
+ */
+public enum Themes {
+    Plain, Ocean;
+}
